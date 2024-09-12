5d4aa4f3908947c011a7ec276e569c89e658a413
Sung Chul Park <jasonsc@naver.com>
2024-09-12 23:05:54 +0900

    Docs : 07.py
    다른 문제풀이 방법 추가 - solution2

--- solution/07.py
+++ solution/07.py
@@ -29,4 +29,38 @@ def solution(dirs):
     answer = len(roads.keys())
     return answer
 
+
 # 시간 복잡도 : O(N)
+
+# 다른 풀이
+# 함수를 분리
+# 저장하는 방식으로 set + 튜플 사용
+
+def is_valid_move(nx, ny):
+    return 0 <= nx < 11 and 0 <= ny < 11
+
+
+def update_location(x, y, dir):
+    if dir == "U":
+        nx, ny = x, y + 1
+    if dir == "D":
+        nx, ny = x, y - 1
+    if dir == "L":
+        nx, ny = x - 1, y
+    if dir == "R":
+        nx, ny = x + 1, y
+    return nx, ny
+
+
+def solution2(dirs):
+    x, y = 5, 5
+    ans = set()  # 겹치는 좌표를 제거
+    for dir in dirs:
+        nx, ny = update_location(x, y, dir)
+        if not is_valid_move(nx, ny):
+            continue
+        ans.add((x, y, nx, ny))
+        ans.add((nx, ny, x, y))  # 반대의 경우도 추가 (경로에는 방향성이 없기 때문)
+        x, y = nx, ny
+    return len(ans) / 2
+
