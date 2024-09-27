f2fb5b6273214134deb88c0cd2d198f55a0a93cb
Sung Chul Park <jasonsc@naver.com>
2024-09-27 13:57:58 +0900

    Add : 21.py - 다른 풀이 추가

--- solution/21.py
+++ solution/21.py
@@ -26,10 +26,32 @@ def solution(want, number, discount):
     return answer
 
 
+def solution2(want, number, discount):
+    want_dict = {}
+    for i in range(len(want)):
+        want_dict[want[i]] = number[i]
+
+    answer = 0
+
+    for i in range(len(discount) - 9):
+        discount_10d = {}
+
+        for j in range(i, i + 10):
+            # get 메서드 사용
+            # discount[j]가 있으면 그 값을 가져오고
+            # 값이 없으면 0을 할당하여 1을 더해준다
+            discount_10d[discount[j]] = discount_10d.get(discount[j], 0) + 1
+
+        # 두 딕셔너리 비교할 때
+        # 순서 상관없이 모든 키-값 쌍이 갑으면 True고 아니면 False
+        if discount_10d == want_dict:
+            answer += 1
+
+    return answer
 
 
-print(solution(["banana", "apple", "rice", "pork", "pot"], [3, 2, 2, 2, 1],
-               ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot",
-                "banana", "apple", "banana"]))
+print(solution2(["banana", "apple", "rice", "pork", "pot"], [3, 2, 2, 2, 1],
+                ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot",
+                 "banana", "apple", "banana"]))
 print(solution(["apple"], [10],
                ["banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana"]))
