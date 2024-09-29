38ce90ccb4b003be9db4d013dc10b20d38d8b91f
Sung Chul Park <jasonsc@naver.com>
2024-09-29 14:58:12 +0900

    Add : 25.py - 다른 풀이 추가

--- solution/25.py
+++ solution/25.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from collections import Counter
 
 
 def solution(orders, course):
@@ -42,6 +43,23 @@ def solution(orders, course):
     return sorted(answer)
 
 
-print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]))
+def solution2(orders, course):
+    answer = []
+    for c in course:  # 각 코스 요리 길에 대해
+        menu = []
+        for order in orders:  # 모든 주문에 대해
+            comb = combinations(sorted(order), c)  # 조합을 이용해 가능한 메뉴 구성을 모두 구함
+            menu += comb
+
+        counter = Counter(menu)  # 각 메뉴 구성이 몇 번 주문되었는지 세어줌
+
+        if len(counter) != 0 and max(counter.values()) != 1:  # 가장 많이 주문된 구성이 1번 이상 주문된 경우
+            for m, cnt in counter.items():
+                if cnt == max(counter.values()):  # 가장 많이 주문된 구성을 찾아서
+                    answer.append("".join(m))
+    return sorted(answer)
+
+
+print(solution2(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]))
 print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5]))
 print(solution(["XYZ", "XWY", "WXA"], [2, 3, 4]))
